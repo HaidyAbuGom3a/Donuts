0f6a89a76af9a914135a570a71787308f035a0c9
Haidy <haidyabugom3a@gmail.com>
2023-07-13 01:31:57 +0300

    add colors

--- app/src/main/java/com/example/donuts/ui/theme/Color.kt
+++ app/src/main/java/com/example/donuts/ui/theme/Color.kt
@@ -9,3 +9,20 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF000000)
+val Black80 = Color(0x50000000)
+val Black60 = Color(0x3C000000)
+val White = Color(0xFFFFFFFF)
+
+val Primary300 = Color(0xFFFF7074)
+val Primary200 = Color(0xFFFF9494)
+val Primary100 = Color(0xFFFED8DF)
+
+val Secondary = Color(0xFF585858)
+
+val CardBlue = Color(0xFFD7E4F6)
+val CardPink = Color(0xFFFFC7D0)
+
+
+
