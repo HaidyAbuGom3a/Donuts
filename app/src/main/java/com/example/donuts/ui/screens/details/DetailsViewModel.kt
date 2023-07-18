a8a59febe06485cc16b639ad5a7a6a450f5b3f6f
Haidy <haidyabugom3a@gmail.com>
2023-07-18 04:10:03 +0300

    implement details view model

--- app/src/main/java/com/example/donuts/ui/screens/details/DetailsViewModel.kt
+++ app/src/main/java/com/example/donuts/ui/screens/details/DetailsViewModel.kt
@@ -1,4 +1,36 @@
 package com.example.donuts.ui.screens.details
 
-class DetailsViewModel {
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
+import com.example.donuts.domain.usecases.GetDonutDetailsUseCase
+import com.example.donuts.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailsViewModel @Inject constructor(
+    private val getDonutDetailsUseCase: GetDonutDetailsUseCase,
+    savedStateHandle: SavedStateHandle
+) :
+    BaseViewModel<DetailsUiState>(DetailsUiState()) {
+    private val args = DetailsArgs(savedStateHandle)
+
+    init {
+        getDonutDetails()
+    }
+
+    private fun getDonutDetails() {
+        val donut: DetailsUiState = getDonutDetailsUseCase(args.id.toInt()).toDetailsUiState()
+        //Log.e("haidy",donut.donutName)
+        _state.update {
+            it.copy(
+                donutName = donut.donutName,
+                image = donut.image,
+                price = donut.price,
+                description = donut.description
+            )
+        }
+    }
+
 }
