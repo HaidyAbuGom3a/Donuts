075b5679d1d258c0f06ab5cd57e539aebee69424
Haidy <haidyabugom3a@gmail.com>
2023-07-18 04:12:59 +0300

    init details screen

--- app/src/main/java/com/example/donuts/ui/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/donuts/ui/screens/details/DetailsScreen.kt
@@ -1,2 +1,13 @@
 package com.example.donuts.ui.screens.details
 
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+
+@Composable
+fun DetailsScreen(
+    navController: NavController,
+    viewModel: DetailsViewModel = hiltViewModel()
+){
+
+}
