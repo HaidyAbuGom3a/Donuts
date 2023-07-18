bc0b69736548d787288b52721e5ef51f847a7f2f
Haidy <haidyabugom3a@gmail.com>
2023-07-18 04:09:45 +0300

    add home, details ui state

--- app/src/main/java/com/example/donuts/ui/screens/details/DetailsUiState.kt
+++ app/src/main/java/com/example/donuts/ui/screens/details/DetailsUiState.kt
@@ -1,2 +1,18 @@
 package com.example.donuts.ui.screens.details
 
+import androidx.annotation.DrawableRes
+import com.example.donuts.domain.entities.DonutEntity
+
+data class DetailsUiState(
+    val donutName: String = "",
+    val description: String = "",
+    @DrawableRes val image: Int = 0,
+    val price: Int = 0
+)
+
+fun DonutEntity.toDetailsUiState() = DetailsUiState(
+    donutName = name ?: "",
+    description = description ?: "",
+    image = image ?: 0,
+    price = newPrice ?: 0,
+    )
--- app/src/main/java/com/example/donuts/ui/screens/home/HomeUiState.kt
+++ app/src/main/java/com/example/donuts/ui/screens/home/HomeUiState.kt
@@ -1,2 +1,28 @@
 package com.example.donuts.ui.screens.home
 
+import androidx.annotation.DrawableRes
+import com.example.donuts.domain.entities.DonutEntity
+
+data class HomeUiState(
+    val donuts: List<DonutUiState> = emptyList(),
+    val offers: List<DonutUiState> = emptyList()
+)
+
+data class DonutUiState(
+    val id: Int = 0,
+    val name: String = "",
+    val description: String = "",
+    @DrawableRes val image: Int = 0,
+    val oldPrice:Int = 0,
+    val newPrice:Int = 0,
+    val isFavorite:Boolean = false
+)
+
+fun DonutEntity.toDonutUiState() = DonutUiState(
+    id = id,
+    name = name ?: "",
+    description = description ?: "",
+    oldPrice = oldPrice ?: 0,
+    newPrice = newPrice ?: 0,
+    image = image ?: 0
+)
