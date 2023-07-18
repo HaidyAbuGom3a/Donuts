ebcc68c79b4fc2d17e32ce60a0605a2db06f44dd
Haidy <haidyabugom3a@gmail.com>
2023-07-18 04:07:13 +0300

    add needed spacers

---
+++ app/src/main/java/com/example/donuts/Composables/HorizontalSpacer84.kt
@@ -0,0 +1,13 @@
+package com.example.donuts.Composables
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.donuts.ui.spacing
+
+@Composable
+fun HorizontalSpacer84(){
+    Spacer(modifier = Modifier.width(MaterialTheme.spacing.spacing_84))
+}
