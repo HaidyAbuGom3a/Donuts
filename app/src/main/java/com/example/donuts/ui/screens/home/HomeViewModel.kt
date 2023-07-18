a95da163624c1d6ad88ad4bb26bec23258f8c649
Haidy <haidyabugom3a@gmail.com>
2023-07-18 04:09:17 +0300

    implement home view model

--- app/src/main/java/com/example/donuts/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/example/donuts/ui/screens/home/HomeViewModel.kt
@@ -1,4 +1,46 @@
 package com.example.donuts.ui.screens.home
 
-class HomeViewModel {
+import com.example.donuts.domain.usecases.GetAllDonutsUseCase
+import com.example.donuts.domain.usecases.GetOffersUseCase
+import com.example.donuts.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val getAllDonutsUseCase: GetAllDonutsUseCase,
+    private val getOffersUseCase: GetOffersUseCase
+) : BaseViewModel<HomeUiState>(HomeUiState()) {
+
+    init {
+        getHomeData()
+    }
+
+    private fun getHomeData() {
+        val donuts: List<DonutUiState> = getAllDonutsUseCase().map { it.toDonutUiState() }
+        val offers: List<DonutUiState> = getOffersUseCase().map { it.toDonutUiState() }
+        _state.update {
+            it.copy(
+                donuts = donuts,
+                offers = offers
+            )
+        }
+    }
+
+    fun onClickFav(id: Int) {
+        val isFavorite = state.value.offers.find { it.id == id }!!.isFavorite
+        _state.update {
+            it.copy(
+                offers = it.offers.map { offer ->
+                    if (offer.id == id) {
+                        offer.copy(isFavorite = !isFavorite)
+                    } else {
+                        offer
+                    }
+                }
+            )
+        }
+    }
+
 }
