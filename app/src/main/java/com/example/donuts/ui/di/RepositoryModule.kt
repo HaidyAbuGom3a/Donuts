60b82957aff4befdac6d20244f0a62400566ac1d
Haidy <haidyabugom3a@gmail.com>
2023-07-18 04:02:47 +0300

    add repository module

---
+++ app/src/main/java/com/example/donuts/ui/di/RepositoryModule.kt
@@ -0,0 +1,23 @@
+package com.example.donuts.ui.di
+
+import com.example.donuts.data.local.DataSource
+import com.example.donuts.data.local.DataSourceImp
+import com.example.donuts.data.repository.RepositoryImp
+import com.example.donuts.domain.repository.Repository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object RepositoryModule {
+    @Singleton
+    @Provides
+    fun bindRepository(
+        dataSource: DataSourceImp,
+    ): Repository {
+        return RepositoryImp(dataSource)
+    }
+}
