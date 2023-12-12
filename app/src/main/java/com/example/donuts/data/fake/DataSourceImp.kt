fd6bf85b6fd18924ebb867dd001ec1fab9cac74f
haidy <haidyabugom3a@gmail.com>
2023-12-12 02:06:59 +0200

    enhance data layer

--- app/src/main/java/com/example/donuts/data/fake/DataSourceImp.kt
+++ app/src/main/java/com/example/donuts/data/fake/DataSourceImp.kt
@@ -1,71 +1,70 @@
-package com.example.donuts.data.local
+package com.example.donuts.data.fake
 
-import com.example.donuts.R
-import com.example.donuts.domain.entities.DonutEntity
+import com.example.donuts.domain.entities.Donut
 import javax.inject.Inject
 
-class DataSourceImp @Inject constructor() : DataSource {
+class DataSourceImp @Inject constructor() : IDataSource {
 
-    override fun getAllDonuts() = donuts
+    override suspend fun getAllDonuts() = donuts
 
-    override fun getAllOffers() = donutsOffer
+    override suspend fun getAllOffers() = donutsOffer
 
-    private val donuts: List<DonutEntity> = listOf(
-        DonutEntity(
-            id = 0,
+    private val donuts: List<Donut> = listOf(
+        Donut(
+            id = "0",
             name = "Chocolate Cherry",
+            price = 22.0,
+            discountPercentage = 0.0,
             hasOffer = false,
-            oldPrice = null,
-            newPrice = 22,
-            image = R.drawable.doughnut_chocolate_cherry_drizzle,
+            imageUrl = "https://i.imgur.com/9eSzuQX.png",
             description = "Experience the delightful fusion of chocolate and cherry with our Chocolate Cherry donuts. Enjoy a tender chocolate dough filled with sweet and tangy cherry, topped with a drizzle of smooth chocolate glaze and cherry bits."
         ),
-        DonutEntity(
-            id = 1,
+        Donut(
+            id = "1",
             name = "Strawberry Rain",
+            price = 22.0,
+            discountPercentage = 0.0,
             hasOffer = false,
-            oldPrice = null,
-            newPrice = 22,
-            image = R.drawable.doughnut_strawberry_rain_sprinkles,
+            imageUrl = "https://i.imgur.com/7aDgI8C.png",
             description = "Savor the refreshing sweetness of our Strawberry Rain donuts. These airy yeast donuts are adorned with a delicate glaze made from ripe strawberries, providing a burst of vibrant flavor."
         ),
-        DonutEntity(
-            id = 2,
+        Donut(
+            id = "2",
             name = "Strawberry",
+            price = 22.0,
+            discountPercentage = 0.0,
             hasOffer = false,
-            oldPrice = null,
-            newPrice = 22,
-            image = R.drawable.doughnut_strawberry_chocolate_drizzle,
+            imageUrl = "https://i.imgur.com/KklGjje.png",
             description = "Delight in the simple pleasure of our Strawberry donuts. Moist and cake-like, these treats are infused with fresh strawberry flavor and topped with a creamy strawberry glaze. Perfect for a sweet morning or afternoon indulgence."
         ),
-        DonutEntity(
-            id = 3,
+        Donut(
+            id = "3",
             name = "Strawberry Wheel",
-            oldPrice = 20,
-            newPrice = 16,
+            price = 22.0,
+            discountPercentage = 0.27,
             hasOffer = true,
-            image = R.drawable.doughnut_strawberry_wheel_sprinkles,
+            imageUrl = "https://i.imgur.com/TO43FGz.png",
             description = "These soft, cake-like Strawberry Frosted Donuts feature fresh strawberries and a delicious fresh strawberry glaze frosting. Pretty enough for company and the perfect treat to satisfy your sweet tooth."
         )
     )
 
-    private val donutsOffer: List<DonutEntity> = listOf(
-        DonutEntity(
-            id = 3,
+    private val donutsOffer: List<Donut> = listOf(
+        Donut(
+            id = "3",
             name = "Strawberry Wheel",
-            oldPrice = 20,
-            newPrice = 16,
+            price = 22.0,
+            discountPercentage = 0.27,
             hasOffer = true,
-            image = R.drawable.doughnut_strawberry_wheel_sprinkles,
+            imageUrl = "https://i.imgur.com/TO43FGz.png",
             description = "These soft, cake-like Strawberry Frosted Donuts feature fresh strawberries and a delicious fresh strawberry glaze frosting. Pretty enough for company and the perfect treat to satisfy your sweet tooth."
         ),
-        DonutEntity(
-            id = 4,
+        Donut(
+            id = "4",
             name = "Chocolate Glaze",
-            oldPrice = 20,
-            newPrice = 16,
+            price = 22.0,
+            discountPercentage = 0.27,
             hasOffer = true,
-            image = R.drawable.doughnut_chocolate_glaze_sprinkles,
+            imageUrl = "https://i.imgur.com/foQWeHB.png",
             description = "Indulge in the rich and velvety goodness of our Chocolate Glaze donuts. These fluffy yeast donuts are coated in a glossy layer of luscious chocolate, creating a decadent treat for chocolate lovers."
         ),
     )
