42d38047c6818cba1ef2ccee269806baf315e959
haidy <haidyabugom3a@gmail.com>
2023-12-12 02:12:20 +0200

    prepare firebase auth requests

--- app/src/main/java/com/example/donuts/ui/di/RepositoryModule.kt
+++ app/src/main/java/com/example/donuts/ui/di/RepositoryModule.kt
@@ -1,8 +1,15 @@
 package com.example.donuts.ui.di
 
-import com.example.donuts.data.local.DataSource
-import com.example.donuts.data.repository.RepositoryImp
-import com.example.donuts.domain.repository.Repository
+import com.example.donuts.data.fake.DataSourceImp
+import com.example.donuts.data.local.IDataStore
+import com.example.donuts.data.repository.AuthRepositoryImp
+import com.example.donuts.data.repository.DonutsRepositoryImp
+import com.example.donuts.data.repository.UserRepositoryImp
+import com.example.donuts.domain.repository.IAuthRepository
+import com.example.donuts.domain.repository.IUserRepository
+import com.example.donuts.domain.repository.IDonutsRepository
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,8 +22,27 @@ internal object RepositoryModule {
     @Singleton
     @Provides
     fun bindRepository(
-        dataSource: DataSource,
-    ): Repository {
-        return RepositoryImp(dataSource)
+        dataSource: DataSourceImp,
+        firestore: FirebaseFirestore
+    ): IDonutsRepository {
+        return DonutsRepositoryImp(dataSource, firestore)
+    }
+
+    @Singleton
+    @Provides
+    fun bindAuthRepository(
+        firebaseAuth: FirebaseAuth,
+        firestore: FirebaseFirestore
+    ): IAuthRepository {
+        return AuthRepositoryImp(firebaseAuth, firestore)
+    }
+
+    @Singleton
+    @Provides
+    fun bindUserRepository(
+        firestore: FirebaseFirestore,
+        dataStore: IDataStore
+    ): IUserRepository {
+        return UserRepositoryImp(firestore, dataStore)
     }
 }
